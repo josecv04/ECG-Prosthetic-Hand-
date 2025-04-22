737c35b59f3ee754d4b950aa292b69f94f3f94b8
josecv04 <bjoe.cv@gmail.com>
2025-04-21 17:15:52 -0700

    Update test_motor.ino

--- code/test_motor.ino
+++ code/test_motor.ino
@@ -1,1 +1,16 @@
+#include <Servo.h> 
+Servo servo1;
 
+int servoPin1 = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+servo1.attach(servoPin1);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+servo1.write(0);
+delay(5000);
+servo1.write(180);
+delay(5000);
+}
